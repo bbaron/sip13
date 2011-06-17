f50446cc8c157aebf804a84f30db9d2e56e28e0a
Willie Wheeler <willie.wheeler@gmail.com>
2011-06-16 22:56:53 -0700

    Adding Java sources and untracking control files.

---
+++ helpdesk/src/main/java/com/springinpractice/ch15/helpdesk/integration/TicketGateway.java
@@ -0,0 +1,18 @@
+/* 
+ * $Id$
+ * 
+ * Copyright (c) 2008-2011 Manning Publications Co.
+ * 
+ * Book web site   - http://www.manning.com/wheeler/
+ * Book blog       - http://springinpractice.com/
+ * Author web site - http://wheelersoftware.com/
+ */
+package com.springinpractice.ch15.helpdesk.integration;
+
+/**
+ * @version $Id$
+ * @author Willie Wheeler (willie.wheeler@gmail.com)
+ */
+public interface TicketGateway {
+
+}
